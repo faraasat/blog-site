47d90e0dac4fc7db4ad20be2864720ec86574042
Farasat Ali <farasataliazeemi1@gmail.com>
2021-03-14 01:34:31 +0500

    Completed the App

--- src/templates/posts/post.template.tsx
+++ src/templates/posts/post.template.tsx
@@ -1,23 +1,44 @@
 import React from "react"
 import LayoutComponent from "../../components/layout/layout.component"
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
+import cuid from "cuid"
 import "./post.styles.css"
 
 const PostTemplate = ({ pageContext }) => {
   const { postDetails } = pageContext
+  const postArray = JSON.parse(postDetails.content.raw).content.map(post => {
+    return post
+  })
 
   return (
     <LayoutComponent>
-      <h1>{postDetails.title}</h1>
-      <div>
-        {documentToReactComponents(JSON.parse(postDetails.content.raw).content[0])}
-        {documentToReactComponents(JSON.parse(postDetails.content.raw).content[1])}
-        {documentToReactComponents(JSON.parse(postDetails.content.raw).content[2])}
-        {documentToReactComponents(JSON.parse(postDetails.content.raw).content[3])}
-        {documentToReactComponents(JSON.parse(postDetails.content.raw).content[4])}
-        {documentToReactComponents(JSON.parse(postDetails.content.raw).content[5])}
-        {documentToReactComponents(JSON.parse(postDetails.content.raw).content[6])}
-        {documentToReactComponents(JSON.parse(postDetails.content.raw).content[7])}
+      <div className="blog-layout__post">
+        <div className="blog-layout__post__box">
+          <h1 className="blog-layout__post__title">{postDetails.title}</h1>
+          <div className="blog-layout__post__box-details">
+            <p>
+              <b>By: </b>
+              {postDetails.author}
+            </p>
+            <p>
+              <b>Updated At: </b>
+              {postDetails.updatedAt}
+            </p>
+          </div>
+        </div>
+        <div className="blog-layout__post__img-container">
+          <img
+            src={postDetails.image.file.url}
+            alt={postDetails.image.description}
+            width="800"
+            height="350"
+          />
+        </div>
+        <div className="blog-layout__post__content">
+          {postArray.map(post => {
+            return <span key={cuid()}>{documentToReactComponents(post)}</span>
+          })}
+        </div>
       </div>
     </LayoutComponent>
   )
