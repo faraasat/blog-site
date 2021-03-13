466d1691e9fcc77f060cc62f2e7d3c375f6bec87
Farasat Ali <farasataliazeemi1@gmail.com>
2021-03-13 16:00:19 +0500

    Created Footer and Updated Other Components

---
+++ src/components/footer/footer.component.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import "./footer.styles.css"
+
+const FooterComponent = () => {
+  return (
+    <footer className="blog-component__footer">
+      <h2>Copyright &copy; Blog Site. All Rights Reserved!</h2>
+    </footer>
+  )
+}
+
+export default FooterComponent
--- src/components/layout/layout.component.tsx
+++ src/components/layout/layout.component.tsx
@@ -2,12 +2,14 @@ import * as React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import "./layout.styles.css"
 import HeaderComponent from "../header/header.component"
+import FooterComponent from "../footer/footer.component"
 
 const Layout = ({ children }) => {
   return (
     <>
       <HeaderComponent />
       <main className="blog-component__layout-main">{children}</main>
+      <FooterComponent />
     </>
   )
 }
