837ac69cad287ec83bc4a47a56a6b2ad5684f6ed
Farasat Ali <farasataliazeemi1@gmail.com>
2021-03-13 16:59:52 +0500

    Updated Gatsby Config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,7 @@
+require("dotenv").config({
+  path: `.env`,
+})
+
 module.exports = {
   siteMetadata: {
     title: `Gatsby Blog Site`,
@@ -14,6 +18,25 @@ module.exports = {
     //     path: `${__dirname}/src/images`,
     //   },
     // },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `wl3m4ipieaqv`,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/blog.svg`,
+      },
+    },
     `gatsby-plugin-gatsby-cloud`,
     // `gatsby-plugin-offline`,
   ],
